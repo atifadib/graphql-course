02953d4d338a53d0edbe19f8ad71207c61066ba2
atifadib <atif.adib25@gmail.com>
2023-08-11 19:53:03 +0100

    adding code for mutation and updates

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from graphene import Schema, ObjectType, String, Int, Field, List
+from graphene import Schema, ObjectType, String, Int, Field, List, Mutation
 
-class userType(ObjectType):
+"""
+Fields Stored in the User
+"""
+class UserType(ObjectType):
     id = Int()
     name = String()
     age = Int()
 
+"""
+Defines how Data will be queried
+"""
 class Query(ObjectType):
-    user = Field(userType, user_id=Int())
-    users_by_min_age = List(userType, min_age=Int())
+    user = Field(UserType, user_id=Int())
+    users_by_min_age = List(UserType, min_age=Int())
 
     users = [
         {"id": 1, "name": "atif", "age": 30},
@@ -26,10 +32,58 @@ class Query(ObjectType):
     def resolve_user(root, info, user_id):
         matches = [user for user in Query.users if user["id"] == user_id]
         return matches[0] if matches else None
+    
+"""
+Create User Mutation Methods
+"""
+class CreateUser(Mutation):
+    class Arguments:
+        name = String()
+        age = Int()
 
-schema = Schema(query=Query)
+    user = Field(UserType)
+    
+    @staticmethod
+    def mutate(root, info, name, age):
+        user = {"id": len(Query.users) + 1, "name": name, "age": age}
+        Query.users.append(user)
+        return CreateUser(user=user)
+    
+"""
+Update User Mutation Methods
+"""
+class UpdateUser(Mutation):
+    class Arguments:
+        id = Int(required=True)
+        name = String()
+        age = Int()
 
-gql = """
+    user = Field(UserType)
+
+    @staticmethod
+    def mutate(root, info, id, name=None, age=None):
+        user = None
+        for _ in Query.users:
+            if _['id'] == id:
+                user = _
+                break
+        if user is not None:
+            user['name'] = name if name else user['name']
+            user['age'] = age if age else user['age']
+            return UpdateUser(user=user)
+        
+        return None    
+"""
+Mutation Define how data will be updated or added
+"""
+class Mutation(ObjectType):
+    create_user = CreateUser.Field()
+    update_user = UpdateUser.Field()
+
+
+schema = Schema(query=Query, mutation=Mutation)
+
+gql1 = """
 query getUsers{
    usersByMinAge(minAge: 300){
         id
@@ -39,6 +93,34 @@ query getUsers{
 }
 """
 
+gql2 = """
+mutation createUser{
+    createUser(name: "Atif", age: 28){
+        user{
+            id
+            name
+            age
+        }
+    }
+}
+"""
+
+gql3 = """
+mutation updateUser{
+    updateUser(id: 6, age: 27){
+        user{
+            id
+            name
+            age
+        }
+    }
+}
+"""
+
 if __name__ == "__main__":
-    output = schema.execute(gql, root_value='Root-Value-Dummy')
+    output = schema.execute(gql1, root_value='Root-Value-Dummy')
+    print(output)
+    output = schema.execute(gql2)
+    print(output)
+    output = schema.execute(gql3)
     print(output)
